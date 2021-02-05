db418432c57a102add3478b791b2889fa032dbaa
Fabian Brandlmayer <f.brandlmayer@gmail.com>
2021-02-05 08:44:02 +0100

    Change insert statement to accomodate new table scheme

--- insert-data.php
+++ insert-data.php
@@ -12,13 +12,12 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = 	"INSERT INTO reflexion (goal_reached, volume_and_concentration, mood, program, average, comments) " .
+$sql = 	"INSERT INTO reflexion (goal_reached, volume_and_concentration, mood, program, comments) " .
 		"VALUES (" . 
 		floatval($_POST['goal_reached']) . "," . 
 		floatval($_POST['volume_and_concentration']) . "," . 
 		floatval($_POST['mood']) . "," . 
 		floatval($_POST['program']) . "," . 
-		floatval($_POST['average']) . "," . 
 		"'" . $_POST['comments'] . "');";
 		
 $result = $conn->query($sql);
