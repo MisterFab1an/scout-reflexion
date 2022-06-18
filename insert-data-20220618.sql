9f4d9bb7fdf8fac15dd5de6193945e6335a12184
Fabian Brandlmayer <f.brandlmayer@gmail.com>
2022-06-18 22:57:12 +0200

    :art: Added data

---
+++ insert-data-20220618.sql
@@ -0,0 +1,14 @@
+INSERT INTO `reflexion` VALUES (2,'2021-01-13 00:00:00',1.50,1.36,1.36,1.91,'Programm: 
+Sommerlagerplatzanfragen besprochen, Sozialprojekt brainstorming, Gruppenkurier-Idee anonyme Fragen an 
+RoRakel\r\nAnmerkungen: wir sind super; hat gut funktioniert; Judith wunderts, dass wir so wenige 
+waren'),(3,'2021-01-06 00:00:00',1.09,1.64,1.91,2.18,NULL),(4,'2020-12-30 
+00:00:00',2.11,1.56,1.67,1.56,NULL),(5,'2020-12-23 00:00:00',1.38,1.50,1.36,1.21,NULL),(6,'2020-12-16 
+00:00:00',1.64,1.27,1.18,1.36,NULL),(7,'2020-12-09 00:00:00',1.00,1.18,1.18,1.09,NULL),(8,'2020-12-02 
+00:00:00',1.21,1.57,1.86,2.00,NULL),(9,'2020-11-25 00:00:00',3.14,2.07,1.64,2.57,NULL),(10,'2020-11-18 
+00:00:00',2.60,2.00,2.00,2.27,NULL),(11,'2021-01-20 20:17:02',1.23,1.92,2.08,2.46,'Programm: SoLa Lagerplatz 
+Abstimmung, Sozialaktion/en festlegen\nWortmeldungen: nix'),(12,'2021-01-27 
+13:21:43',0.00,1.36,1.17,1.17,''),(13,'2021-02-02 00:00:00',1.00,1.15,1.54,1.62,'Programm: Doku Masdar City 
+(Pia), plaudern;\r\nWortmeldungen: interessant, lehrreich, futuristisch, inspirierend, 
+lästern;'),(14,'2021-02-10 20:36:18',1.00,1.78,1.89,1.33,'Programm: Kekse backen\nWortmeldungen: 
+italienischer Hintergrund wurde appreciated'),(15,'2021-02-24 20:18:30',2.73,1.73,1.64,2.27,'Programm: 
+Rottenrat beenden - Ziele setzen');
